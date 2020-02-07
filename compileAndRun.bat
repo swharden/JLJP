c64f092fd4f2e6098c4b16f39d6220be4627f56d
Scott W Harden <swharden@gmail.com>
2020-02-07 13:25:33 -0500

    Update compileAndRun.bat

--- compileAndRun.bat
+++ compileAndRun.bat
@@ -1,24 +1,28 @@
 @echo off
 
+:: ensure java.exe can be found
 java -version >nul 2>&1 && (
     echo Java is installed: & java -version & @echo.
 ) || (
     GOTO ERROR_NO_JAVA
 )
 
+:: ensure javac.exe can be found
 javac -version >nul 2>&1 && (
     echo Java compiler is installed: & javac -version
 ) || (
     GOTO ERROR_NO_JAVC
 )
 
-:: launch the java gui
+:: recompile everything
 echo.
 echo Compiling JLJP application...
 cd src
 del *.class
 javac -nowarn Jljp.java
 pause
+
+:: launch the GUI
 java Jljp.java
 exit
 
