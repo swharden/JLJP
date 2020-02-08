d1c003ba83d1d18bd99dee3fb21508f93c21dc7d
Scott W Harden <swharden@gmail.com>
2020-02-07 20:31:04 -0500

    improved layout and added window title

--- src/Jljp.java
+++ src/Jljp.java
@@ -1,26 +1,25 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
-import java.awt.event.*;
-import java.awt.font.*;
-import java.awt.geom.*;
-import java.applet.*;
-import java.text.*;
 
 /** Graphical interface for the Jljp classes. */
 
 public class Jljp extends JFrame {
 
 	public Jljp() {
-		setSize(1000, 600);
+		this.setTitle("JLJP by Doriano Brogioli (Version 2.0.1)");
+		setSize(900, 600);
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		this.setLocation(dim.width / 2 - this.getSize().width / 2, dim.height / 2 - this.getSize().height / 2);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		IonSetPanel is = new IonSetPanel();
-		LJPPanel p = new LJPPanel(is, null);
-		getContentPane().add(p);
+
+		LJPPanel ljpPanel = new LJPPanel(new IonSetPanel(), null);
+		ljpPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+		getContentPane().add(ljpPanel, BorderLayout.PAGE_START);
 	}
 
 	public static void main(String[] args) {
 		Jljp j = new Jljp();
-		j.setTitle("JLJP by Doriano Brogioli");
 		j.setVisible(true);
 	}
 
