513c766028ecf53e67b005978d03cd4e8b421a08
Scott W Harden <swharden@gmail.com>
2020-02-08 12:06:11 -0500

    add tests for ion name case sensitivity #1

--- test/CalculationTest.java
+++ test/CalculationTest.java
@@ -11,4 +11,21 @@ public class CalculationTest {
         assertTrue(collection.isEmpty());
     }
 
+    @Test
+    public void test_ion_nameLookupWorks() {
+        Ion Zn = new Ion("Zn");
+        Ion Default = new Ion("lolz");
+        assertTrue(Zn.getMu() != Default.getMu());
+    }
+
+    @Test
+    public void test_ion_nameIsCaseInsensitive() {
+        Ion Zn = new Ion("Zn");
+        Ion ZnUpper = new Ion("ZN");
+        Ion ZnLower = new Ion("zn");
+        assertTrue(Zn.getMu() == ZnUpper.getMu());
+        assertTrue(Zn.getMu() == ZnLower.getMu());
+        assertTrue(Zn.getMu() == ZnLower.getMu());
+    }
+
 }
