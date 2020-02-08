f26e5e10061e18ec2ab1c982fa8fe7d830cbf248
Scott W Harden <swharden@gmail.com>
2020-02-07 19:07:39 -0500

    ignore case when matching ion names
    discussed in #1

--- src/Ion.java
+++ src/Ion.java
@@ -29,391 +29,391 @@ public class Ion {
 		charge = 1;
 		mu = 73.48e-4 / Nav / e / e;
 
-		if (name.equals("Ag")) {
+		if (name.equalsIgnoreCase("Ag")) {
 			charge = +1;
 			mu = 4.01656e+11;
 		}
-		if (name.equals("Al")) {
+		if (name.equalsIgnoreCase("Al")) {
 			charge = +3;
 			mu = 1.31939e+11;
 		}
-		if (name.equals("Au(CN)2")) {
+		if (name.equalsIgnoreCase("Au(CN)2")) {
 			charge = -1;
 			mu = 3.24439e+11;
 		}
-		if (name.equals("Au(CN)4")) {
+		if (name.equalsIgnoreCase("Au(CN)4")) {
 			charge = -1;
 			mu = 2.33596e+11;
 		}
-		if (name.equals("Ba")) {
+		if (name.equalsIgnoreCase("Ba")) {
 			charge = +2;
 			mu = 2.06343e+11;
 		}
-		if (name.equals("B(C6H5)4")) {
+		if (name.equalsIgnoreCase("B(C6H5)4")) {
 			charge = -1;
 			mu = 1.36265e+11;
 		}
-		if (name.equals("Be")) {
+		if (name.equalsIgnoreCase("Be")) {
 			charge = +2;
 			mu = 1.45998e+11;
 		}
-		if (name.equals("Br3")) {
+		if (name.equalsIgnoreCase("Br3")) {
 			charge = -1;
 			mu = 2.79018e+11;
 		}
-		if (name.equals("Br")) {
+		if (name.equalsIgnoreCase("Br")) {
 			charge = -1;
 			mu = 5.06774e+11;
 		}
-		if (name.equals("BrO3")) {
+		if (name.equalsIgnoreCase("BrO3")) {
 			charge = -1;
 			mu = 3.61425e+11;
 		}
-		if (name.equals("Ca")) {
+		if (name.equalsIgnoreCase("Ca")) {
 			charge = +2;
 			mu = 1.92944e+11;
 		}
-		if (name.equals("Cd")) {
+		if (name.equalsIgnoreCase("Cd")) {
 			charge = +2;
 			mu = 1.75197e+11;
 		}
-		if (name.equals("Ce")) {
+		if (name.equalsIgnoreCase("Ce")) {
 			charge = +3;
 			mu = 1.50972e+11;
 		}
-		if (name.equals("Cl")) {
+		if (name.equalsIgnoreCase("Cl")) {
 			charge = -1;
 			mu = 4.95159e+11;
 		}
-		if (name.equals("ClO2")) {
+		if (name.equalsIgnoreCase("ClO2")) {
 			charge = -1;
 			mu = 3.37417e+11;
 		}
-		if (name.equals("ClO3")) {
+		if (name.equalsIgnoreCase("ClO3")) {
 			charge = -1;
 			mu = 4.19176e+11;
 		}
-		if (name.equals("ClO4")) {
+		if (name.equalsIgnoreCase("ClO4")) {
 			charge = -1;
 			mu = 4.36695e+11;
 		}
-		if (name.equals("CN")) {
+		if (name.equalsIgnoreCase("CN")) {
 			charge = -1;
 			mu = 5.06125e+11;
 		}
-		if (name.equals("CNO")) {
+		if (name.equalsIgnoreCase("CNO")) {
 			charge = -1;
 			mu = 4.19176e+11;
 		}
-		if (name.equals("CO3")) {
+		if (name.equalsIgnoreCase("CO3")) {
 			charge = -2;
 			mu = 2.24836e+11;
 		}
-		if (name.equals("Co")) {
+		if (name.equalsIgnoreCase("Co")) {
 			charge = +2;
 			mu = 1.78442e+11;
 		}
-		if (name.equals("[Co(CN)6]")) {
+		if (name.equalsIgnoreCase("[Co(CN)6]")) {
 			charge = -3;
 			mu = 2.13914e+11;
 		}
-		if (name.equals("[Co(NH3)6]")) {
+		if (name.equalsIgnoreCase("[Co(NH3)6]")) {
 			charge = +3;
 			mu = 2.20402e+11;
 		}
-		if (name.equals("Cr")) {
+		if (name.equalsIgnoreCase("Cr")) {
 			charge = +3;
 			mu = 1.44916e+11;
 		}
-		if (name.equals("CrO4")) {
+		if (name.equalsIgnoreCase("CrO4")) {
 			charge = -2;
 			mu = 2.75773e+11;
 		}
-		if (name.equals("Cs")) {
+		if (name.equalsIgnoreCase("Cs")) {
 			charge = +1;
 			mu = 5.00934e+11;
 		}
-		if (name.equals("Cu")) {
+		if (name.equalsIgnoreCase("Cu")) {
 			charge = +2;
 			mu = 1.739e+11;
 		}
-		if (name.equals("D")) {
+		if (name.equalsIgnoreCase("D")) {
 			charge = +1;
 			mu = 1.62155e+12;
 		}
-		if (name.equals("Dy")) {
+		if (name.equalsIgnoreCase("Dy")) {
 			charge = +3;
 			mu = 1.41888e+11;
 		}
-		if (name.equals("Er")) {
+		if (name.equalsIgnoreCase("Er")) {
 			charge = +3;
 			mu = 1.42537e+11;
 		}
-		if (name.equals("Eu")) {
+		if (name.equalsIgnoreCase("Eu")) {
 			charge = +3;
 			mu = 1.46647e+11;
 		}
-		if (name.equals("F")) {
+		if (name.equalsIgnoreCase("F")) {
 			charge = -1;
 			mu = 3.59479e+11;
 		}
-		if (name.equals("FeII")) {
+		if (name.equalsIgnoreCase("FeII")) {
 			charge = +2;
 			mu = 1.75197e+11;
 		}
-		if (name.equals("FeIII")) {
+		if (name.equalsIgnoreCase("FeIII")) {
 			charge = +3;
 			mu = 1.47079e+11;
 		}
-		if (name.equals("[Fe(CN)6]III")) {
+		if (name.equalsIgnoreCase("[Fe(CN)6]III")) {
 			charge = -3;
 			mu = 2.1824e+11;
 		}
-		if (name.equals("[Fe(CN)6]IIII")) {
+		if (name.equalsIgnoreCase("[Fe(CN)6]IIII")) {
 			charge = -4;
 			mu = 1.79091e+11;
 		}
-		if (name.equals("Gd")) {
+		if (name.equalsIgnoreCase("Gd")) {
 			charge = +3;
 			mu = 1.45565e+11;
 		}
-		if (name.equals("H2AsO4")) {
+		if (name.equalsIgnoreCase("H2AsO4")) {
 			charge = -1;
 			mu = 2.20619e+11;
 		}
-		if (name.equals("H2PO2")) {
+		if (name.equalsIgnoreCase("H2PO2")) {
 			charge = -1;
 			mu = 2.98484e+11;
 		}
-		if (name.equals("H2PO4")) {
+		if (name.equalsIgnoreCase("H2PO4")) {
 			charge = -1;
 			mu = 2.33596e+11;
 		}
-		if (name.equals("H2SbO4")) {
+		if (name.equalsIgnoreCase("H2SbO4")) {
 			charge = -1;
 			mu = 2.01152e+11;
 		}
-		if (name.equals("H")) {
+		if (name.equalsIgnoreCase("H")) {
 			charge = +1;
 			mu = 2.2688e+12;
 		}
-		if (name.equals("HCO3")) {
+		if (name.equalsIgnoreCase("HCO3")) {
 			charge = -1;
 			mu = 2.88751e+11;
 		}
-		if (name.equals("HF2")) {
+		if (name.equalsIgnoreCase("HF2")) {
 			charge = -1;
 			mu = 4.86659e+11;
 		}
-		if (name.equals("Hg")) {
+		if (name.equalsIgnoreCase("Hg")) {
 			charge = +2;
 			mu = 2.22565e+11;
 		}
-		if (name.equals("Ho")) {
+		if (name.equalsIgnoreCase("Ho")) {
 			charge = +3;
 			mu = 1.43402e+11;
 		}
-		if (name.equals("HPO4")) {
+		if (name.equalsIgnoreCase("HPO4")) {
 			charge = -2;
 			mu = 1.8493e+11;
 		}
-		if (name.equals("HS")) {
+		if (name.equalsIgnoreCase("HS")) {
 			charge = -1;
 			mu = 4.21771e+11;
 		}
-		if (name.equals("HSO3")) {
+		if (name.equalsIgnoreCase("HSO3")) {
 			charge = -1;
 			mu = 3.7635e+11;
 		}
-		if (name.equals("HSO4")) {
+		if (name.equalsIgnoreCase("HSO4")) {
 			charge = -1;
 			mu = 3.37417e+11;
 		}
-		if (name.equals("I")) {
+		if (name.equalsIgnoreCase("I")) {
 			charge = -1;
 			mu = 4.98339e+11;
 		}
-		if (name.equals("IO3")) {
+		if (name.equalsIgnoreCase("IO3")) {
 			charge = -1;
 			mu = 2.62796e+11;
 		}
-		if (name.equals("IO4")) {
+		if (name.equalsIgnoreCase("IO4")) {
 			charge = -1;
 			mu = 3.53639e+11;
 		}
-		if (name.equals("K")) {
+		if (name.equalsIgnoreCase("K")) {
 			charge = +1;
 			mu = 4.76796e+11;
 		}
-		if (name.equals("La")) {
+		if (name.equalsIgnoreCase("La")) {
 			charge = +3;
 			mu = 1.50756e+11;
 		}
-		if (name.equals("Li")) {
+		if (name.equalsIgnoreCase("Li")) {
 			charge = +1;
 			mu = 2.50857e+11;
 		}
-		if (name.equals("Mg")) {
+		if (name.equalsIgnoreCase("Mg")) {
 			charge = +2;
 			mu = 1.71953e+11;
 		}
-		if (name.equals("Mn")) {
+		if (name.equalsIgnoreCase("Mn")) {
 			charge = +2;
 			mu = 1.73575e+11;
 		}
-		if (name.equals("MnO4")) {
+		if (name.equalsIgnoreCase("MnO4")) {
 			charge = -1;
 			mu = 3.97763e+11;
 		}
-		if (name.equals("MoO4")) {
+		if (name.equalsIgnoreCase("MoO4")) {
 			charge = -2;
 			mu = 2.41707e+11;
 		}
-		if (name.equals("N2H5")) {
+		if (name.equalsIgnoreCase("N2H5")) {
 			charge = +1;
 			mu = 3.82838e+11;
 		}
-		if (name.equals("N3")) {
+		if (name.equalsIgnoreCase("N3")) {
 			charge = -1;
 			mu = 4.47726e+11;
 		}
-		if (name.equals("Na")) {
+		if (name.equalsIgnoreCase("Na")) {
 			charge = +1;
 			mu = 3.24958e+11;
 		}
-		if (name.equals("N(CN)2")) {
+		if (name.equalsIgnoreCase("N(CN)2")) {
 			charge = -1;
 			mu = 3.53639e+11;
 		}
-		if (name.equals("Nd")) {
+		if (name.equalsIgnoreCase("Nd")) {
 			charge = +3;
 			mu = 1.50107e+11;
 		}
-		if (name.equals("NH2SO3")) {
+		if (name.equalsIgnoreCase("NH2SO3")) {
 			charge = -1;
 			mu = 3.13408e+11;
 		}
-		if (name.equals("NH4")) {
+		if (name.equalsIgnoreCase("NH4")) {
 			charge = +1;
 			mu = 4.76926e+11;
 		}
-		if (name.equals("Ni")) {
+		if (name.equalsIgnoreCase("Ni")) {
 			charge = +2;
 			mu = 1.60922e+11;
 		}
-		if (name.equals("NO2")) {
+		if (name.equalsIgnoreCase("NO2")) {
 			charge = -1;
 			mu = 4.65895e+11;
 		}
-		if (name.equals("NO3")) {
+		if (name.equalsIgnoreCase("NO3")) {
 			charge = -1;
 			mu = 4.63429e+11;
 		}
-		if (name.equals("OCN")) {
+		if (name.equalsIgnoreCase("OCN")) {
 			charge = -1;
 			mu = 4.19176e+11;
 		}
-		if (name.equals("OD")) {
+		if (name.equalsIgnoreCase("OD")) {
 			charge = -1;
 			mu = 7.72166e+11;
 		}
-		if (name.equals("OH")) {
+		if (name.equalsIgnoreCase("OH")) {
 			charge = -1;
 			mu = 1.28478e+12;
 		}
-		if (name.equals("Pb")) {
+		if (name.equalsIgnoreCase("Pb")) {
 			charge = +2;
 			mu = 2.30352e+11;
 		}
-		if (name.equals("PF6")) {
+		if (name.equalsIgnoreCase("PF6")) {
 			charge = -1;
 			mu = 3.69212e+11;
 		}
-		if (name.equals("PO3F")) {
+		if (name.equalsIgnoreCase("PO3F")) {
 			charge = -2;
 			mu = 2.0537e+11;
 		}
-		if (name.equals("PO4")) {
+		if (name.equalsIgnoreCase("PO4")) {
 			charge = -3;
 			mu = 2.0072e+11;
 		}
-		if (name.equals("Pr")) {
+		if (name.equalsIgnoreCase("Pr")) {
 			charge = +3;
 			mu = 1.50324e+11;
 		}
-		if (name.equals("Ra")) {
+		if (name.equalsIgnoreCase("Ra")) {
 			charge = +2;
 			mu = 2.16725e+11;
 		}
-		if (name.equals("Rb")) {
+		if (name.equalsIgnoreCase("Rb")) {
 			charge = +1;
 			mu = 5.04828e+11;
 		}
-		if (name.equals("ReO4")) {
+		if (name.equalsIgnoreCase("ReO4")) {
 			charge = -1;
 			mu = 3.56234e+11;
 		}
-		if (name.equals("Sb(OH)6")) {
+		if (name.equalsIgnoreCase("Sb(OH)6")) {
 			charge = -1;
 			mu = 2.06992e+11;
 		}
-		if (name.equals("Sc")) {
+		if (name.equalsIgnoreCase("Sc")) {
 			charge = +3;
 			mu = 1.39942e+11;
 		}
-		if (name.equals("SCN")) {
+		if (name.equalsIgnoreCase("SCN")) {
 			charge = -1;
 			mu = 4.2826e+11;
 		}
-		if (name.equals("SeCN")) {
+		if (name.equalsIgnoreCase("SeCN")) {
 			charge = -1;
 			mu = 4.19825e+11;
 		}
-		if (name.equals("SeO4")) {
+		if (name.equalsIgnoreCase("SeO4")) {
 			charge = -2;
 			mu = 2.45601e+11;
 		}
-		if (name.equals("Sm")) {
+		if (name.equalsIgnoreCase("Sm")) {
 			charge = +3;
 			mu = 1.48161e+11;
 		}
-		if (name.equals("SO3")) {
+		if (name.equalsIgnoreCase("SO3")) {
 			charge = -2;
 			mu = 2.33596e+11;
 		}
-		if (name.equals("SO4")) {
+		if (name.equalsIgnoreCase("SO4")) {
 			charge = -2;
 			mu = 2.59551e+11;
 		}
-		if (name.equals("Sr")) {
+		if (name.equalsIgnoreCase("Sr")) {
 			charge = +2;
 			mu = 1.92717e+11;
 		}
-		if (name.equals("Tl")) {
+		if (name.equalsIgnoreCase("Tl")) {
 			charge = +1;
 			mu = 4.84712e+11;
 		}
-		if (name.equals("Tm")) {
+		if (name.equalsIgnoreCase("Tm")) {
 			charge = +3;
 			mu = 1.41456e+11;
 		}
-		if (name.equals("UO2")) {
+		if (name.equalsIgnoreCase("UO2")) {
 			charge = +2;
 			mu = 1.03821e+11;
 		}
-		if (name.equals("WO4")) {
+		if (name.equalsIgnoreCase("WO4")) {
 			charge = -2;
 			mu = 2.23863e+11;
 		}
-		if (name.equals("Yb")) {
+		if (name.equalsIgnoreCase("Yb")) {
 			charge = +3;
 			mu = 1.41888e+11;
 		}
-		if (name.equals("Y")) {
+		if (name.equalsIgnoreCase("Y")) {
 			charge = +3;
 			mu = 1.34102e+11;
 		}
-		if (name.equals("Zn")) {
+		if (name.equalsIgnoreCase("Zn")) {
 			charge = +2;
 			mu = 1.71304e+11;
 		}
