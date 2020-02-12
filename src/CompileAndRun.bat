8c5883c2d5aa9bf74edc0715d4b7150dbfe4a599
Scott W Harden <swharden@gmail.com>
2020-02-11 22:15:41 -0500

    improve launch scripts

--- src/CompileAndRun.bat
+++
@@ -1,5 +0,0 @@
-:: Javac.exe and Java.exe must both be in your system path.
-:: Get these from https://www.oracle.com/technetwork/java/javase/downloads/index.html
-del *.class /f /q /s
-javac -nowarn Jljp.java
-java Jljp
