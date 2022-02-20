f9d30a29058eb2880c921e1cea27a0b435cbd1a3
Scott W Harden <swharden@gmail.com>
2022-02-20 15:07:05 -0500

    dev: delete old files

--- dev/annotated-source/EquationSystem.java
+++
@@ -1,7 +0,0 @@
-public interface EquationSystem {
-
-    public void equations(double[] x, double[] f) throws Exception; // equations in the form f(x)=0
-
-    public int number(); // number of equations and unknowns
-
-}
