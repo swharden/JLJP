468720a4c656fd17690076e2ea1fa914fd72533f
Scott W Harden <swharden@gmail.com>
2022-02-20 15:48:41 -0500

    ignore calculation tests

--- src/test/java/CalculationTest.java
+++ src/test/java/CalculationTest.java
@@ -1,7 +1,10 @@
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+
+import org.junit.Ignore;
 import org.junit.Test;
 
+@Ignore
 public class CalculationTest {
 
     @Test
